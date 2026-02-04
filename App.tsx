ebecb909c981c3ffa39c28e6826f97a7bfd9960f
Michał Sęk <michal.sek@swmansion.com>
2026-02-04 15:22:30 +0100

    rumburak

--- App.tsx
+++ App.tsx
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  FishjamProvider,
+  useInitializeDevices,
+} from "@fishjam-cloud/react-native-client";
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+import { CameraPreview } from "./eserce/CameraPreview";
+
+const JamJamID = `8a589a8dca1b40f7a3daf4c3fda68fba`;
+
+const JamFishJamComponent = () => {
+  const [isInitialized, setIsInitialized] = useState(false);
+  const { initializeDevices } = useInitializeDevices();
+
+  console.log("Rendering JamFishJamComponent, isInitialized:", isInitialized);
+
+  return isInitialized ? (
+    <>
+      <Text>Fishjam is initialized!</Text>
+      <CameraPreview />
+    </>
+  ) : (
+    <>
+      <Text>Fishjam is not initialized.</Text>
+      <Button
+        title="Initialize Fishjam"
+        onPress={async () => {
+          const result = await initializeDevices({
+            enableAudio: true,
+            enableVideo: false,
+          });
+
+          console.log("InitializeDevices result:", result);
+          setIsInitialized(true);
+        }}
+      />
+    </>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <FishjamProvider fishjamId={JamJamID}>
+      <View style={styles.container}>
+        <JamFishJamComponent />
+        <StatusBar style="auto" />
+      </View>
+    </FishjamProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
